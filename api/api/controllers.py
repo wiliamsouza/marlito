cb0d7871ddcae17346a8b5a644875d24e480c7d0
Wiliam Souza <wiliamsouza83@gmail.com>
2014-06-21 18:06:01 -0300

    API: Added health endpoint

--- api/api/controllers.py
+++ api/api/controllers.py
@@ -1,27 +1,6 @@
 from . import app
 
 
-@app.route('/ping')
-def login():
-    return 'pong'
-
-
-"""
-@app.route('/upload', methods=['POST'])
-def upload():
-    form = CarForm()
-    if form.validate_on_submit():
-        if 'photo' in request.files:
-            photo = request.files['photo']
-            filename = secure_filename(photo.filename)
-            path = '{0}/{1}'.format(app.config['UPLOAD_FOLDER'], filename)
-            photo.save(path)
-            car = Car(manufacturer=form.manufacturer.data,
-                      model=form.model.data, year=form.year.data,
-                      photo=filename)
-            car.save()
-            flash('New car added')
-            return redirect(url_for('search'))
-    url = url_for('cars')
-    return render_template('cars.html', form=form, url=url)
-"""
+@app.route('/health')
+def health():
+    return 'OK', 200
